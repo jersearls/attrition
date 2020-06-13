b1041cc60db9f650a59925a31a0eab33df5b60d6
jersearls <searls@me.com>
2020-06-13 01:27:34 -0400

    enhance macro, add tests

--- lib/attrition.ex
+++ lib/attrition.ex
@@ -1,36 +1,51 @@
 defmodule Attrition do
   @moduledoc false
 
-  defmacro __using__(_opts) do
-    if configured?() do
-      quoted_helpers()
-    else
-      quoted_noop_helpers()
+  @callback data_qa(value :: any()) :: String.t() | {:safe, String.t()}
+
+  defmacro __using__([]) do
+    quoted_data_qa_fn = do_quoted_data_qa_fn()
+
+    quote do
+      @behaviour Attrition
+
+      alias Attrition
+
+      @impl Attrition
+      unquote(quoted_data_qa_fn)
+
+      defoverridable Attrition
     end
   end
 
-  defp configured? do
+  @spec configured? :: boolean()
+  def configured? do
     :attrition
     |> Application.get_env(:data_qa)
     |> enabled?
   end
 
+  @spec data_qa_string(String.t()) :: {:safe, String.t()}
+  def data_qa_string(value) when is_binary(value) do
+    {:safe, ~s(data-qa="#{value}")}
+  end
+
+  def do_quoted_data_qa_fn() do
+    if configured?(), do: quoted_data_qa(), else: quoted_noop_data_qa()
+  end
+
   defp enabled?(:enabled), do: true
   defp enabled?(_), do: false
 
-  defp quoted_noop_helpers do
+  defp quoted_noop_data_qa do
     quote do
-      alias Attrition
-
       def data_qa(_), do: ""
     end
   end
 
-  defp quoted_helpers do
+  defp quoted_data_qa do
     quote do
-      alias Attrition
-
-      def data_qa(value), do: "data-qa=#{value}"
+      def data_qa(value), do: Attrition.data_qa_string(value)
     end
   end
 end
--- test/attrition_test.exs
+++ test/attrition_test.exs
@@ -1,8 +1,59 @@
 defmodule AttritionTest do
   use ExUnit.Case
-  doctest Attrition
+  use Attrition
 
-  test "greets the world" do
-    assert Attrition.hello() == :world
+  describe "data_qa_string/1" do
+    test "with string value, returns data_qa value escaped string" do
+      assert Attrition.data_qa_string("test") == {:safe, "data-qa=\"test\""}
+    end
+
+    test "with non-string values, raises fn clause errors" do
+      assert_raise(FunctionClauseError, fn ->
+        Attrition.data_qa_string(1)
+      end)
+
+      assert_raise(FunctionClauseError, fn ->
+        Attrition.data_qa_string(:one)
+      end)
+    end
+  end
+
+  describe "configured/0" do
+    setup do
+      on_exit(fn -> data_qa_disabled() end)
+    end
+
+    test "with no configuration, returns false" do
+      refute Attrition.configured?()
+    end
+
+    test "with configuration, returns true" do
+      data_qa_enabled()
+      assert Attrition.configured?()
+    end
+  end
+
+  describe "do_quoted_data_qa_fn" do
+    setup do
+      on_exit(fn -> data_qa_disabled() end)
+    end
+
+    test "with no data_qa enabled, returns quoted_noop_data_qa/0 AST" do
+      assert Attrition.do_quoted_data_qa_fn() |> Macro.to_string() =~ "def(data_qa(_)"
+    end
+
+    test "with data_qa enabled, returns quoted_data_qa/0 AST" do
+      data_qa_enabled()
+
+      assert Attrition.do_quoted_data_qa_fn() |> Macro.to_string() =~ "def(data_qa(value)"
+    end
+  end
+
+  defp data_qa_enabled do
+    Application.put_env(:attrition, :data_qa, :enabled)
+  end
+
+  defp data_qa_disabled do
+    Application.delete_env(:attrition, :data_qa)
   end
 end
